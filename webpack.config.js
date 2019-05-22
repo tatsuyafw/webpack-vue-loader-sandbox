bc0cc34b48a052d32fbd2dd46a4c5d325a7f56a1
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2019-05-23 00:42:46 +0900

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      { test: /\.vue$/, use: 'vue-loader' }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm.js'
+    },
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
